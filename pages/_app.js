12e4e360becef171e3b8dfc8c6069ee8ee50677c
maximdegeyter <maximdegeyter@outlook.com>
2020-11-06 18:33:41 +0100

    added header to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+
+import customTheme from '../styles/theme';
+import Header from '../components/header';
+import Footer from '../components/footer';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CSSReset />
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
