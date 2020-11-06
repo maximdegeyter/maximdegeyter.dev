c4ea539a082e76e6297a357835a50813226cd6d1
maximdegeyter <maximdegeyter@outlook.com>
2020-11-06 18:36:47 +0100

    created projects page

---
+++ pages/projects.js
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+import { Flex, Heading } from '@chakra-ui/core';
+
+export default function Projects() {
+  return (
+    <div>
+      <Head>
+        <title>Maxim De Geyter | Projects</title>
+        <link rel='icon' href='/logo.png' />
+      </Head>
+      <Flex
+        as='main'
+        direction='column'
+        maxWidth='960px'
+        width='100%'
+        mx='auto'
+        my='6'
+      >
+        <Heading as='h1' size='xl' mb='2' color='text.900'>
+          Projects.
+        </Heading>
+        <Text>Coming soon. under construction..</Text>
+      </Flex>
+    </div>
+  );
+}
