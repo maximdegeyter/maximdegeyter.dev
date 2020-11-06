d8f5c0a992014a16267d67ceeee5cf97b84bc3cd
maximdegeyter <maximdegeyter@outlook.com>
2020-11-07 00:35:54 +0100

    add contact section

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import { Flex, Heading, Text } from '@chakra-ui/core';
+import { Box, Flex, Heading, List, ListItem, ListIcon } from '@chakra-ui/core';
+import { MdCheckCircle } from 'react-icons/md';
 
 export default function About() {
   return (
@@ -14,12 +15,79 @@ export default function About() {
         maxWidth='960px'
         width='100%'
         mx='auto'
-        my='6'
+        my='5'
       >
-        <Heading as='h1' size='xl' mb='2' color='text.900'>
+        <Heading
+          as='h2'
+          size='xl'
+          mb='3'
+          color='text.900'
+          fontFamily='lato'
+          fontWeight='900'
+        >
           About.
         </Heading>
-        <Text>Coming soon. under construction..</Text>
+        <Box
+          w={['100%', '100%', '100%', '720px']}
+          p='3'
+          bg='brand.600'
+          borderWidth='1px'
+          borderRadius='8px'
+          borderColor='brand.900'
+          mx='auto'
+          mb='5'
+        >
+          <List spacing={3}>
+            <ListItem>
+              <ListIcon as={MdCheckCircle} color='brand.900' />
+              25 years old
+            </ListItem>
+            <ListItem>
+              <ListIcon as={MdCheckCircle} color='brand.900' />
+              Ghent, Belgium 🇧🇪
+            </ListItem>
+            <ListItem>
+              <ListIcon as={MdCheckCircle} color='brand.900' />
+              Devine Kask graduate
+            </ListItem>
+            <ListItem>
+              <ListIcon as={MdCheckCircle} color='brand.900' />
+              Huge Star Wars geek 🤖
+            </ListItem>
+            <ListItem>
+              <ListIcon as={MdCheckCircle} color='brand.900' />
+              Laker fan
+            </ListItem>
+            <ListItem>
+              <ListIcon as={MdCheckCircle} color='brand.900' />
+              Jamstack
+            </ListItem>
+          </List>
+        </Box>
+        <Box w='100%' py='3' borderBottom='solid 1px' borderColor='text.600'>
+          <Heading
+            as='h3'
+            size='lg'
+            mb='2'
+            color='text.900'
+            fontFamily='lato'
+            fontWeight='900'
+          >
+            Tech Stack
+          </Heading>
+        </Box>
+        <Box w='100%' py='3' borderBottom='solid 1px' borderColor='text.600'>
+          <Heading
+            as='h3'
+            size='lg'
+            mb='2'
+            color='text.900'
+            fontFamily='lato'
+            fontWeight='900'
+          >
+            Comic Books
+          </Heading>
+        </Box>
       </Flex>
     </div>
   );
