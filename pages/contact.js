3658ce4f99f521fd58c217242c687826e5acf585
maximdegeyter <maximdegeyter@outlook.com>
2020-11-07 16:17:48 +0100

    create contact form with formspree

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,40 @@
+import { useState } from 'react';
 import Head from 'next/head';
-import { Flex, Heading, Text } from '@chakra-ui/core';
+import {
+  Flex,
+  Heading,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Stack,
+  Box,
+  Textarea,
+} from '@chakra-ui/core';
 
 export default function Contact() {
+  const [status, setStatus] = useState('');
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        setStatus('SUCCESS');
+      } else {
+        setStatus('ERROR');
+      }
+    };
+    xhr.send(data);
+  };
+
   return (
     <div>
       <Head>
@@ -11,15 +44,65 @@ export default function Contact() {
       <Flex
         as='main'
         direction='column'
+        minHeight='55vh'
         maxWidth='960px'
         width='100%'
         mx='auto'
-        my='6'
+        my='16'
       >
-        <Heading as='h1' size='xl' mb='2' color='text.900'>
+        <Heading as='h1' size='xl' mb='8' color='text.900'>
           Contact.
         </Heading>
-        <Text>Coming soon. under construction..</Text>
+        <Box w={['100%', '100%', '100%', '720px']} mx='auto'>
+          <form
+            onSubmit={submitForm}
+            action='https://formspree.io/f/mpzorejd'
+            method='POST'
+          >
+            <FormControl>
+              <Stack spacing='8'>
+                <Box>
+                  <FormLabel>Email address</FormLabel>
+                  <Input
+                    type='email'
+                    name='email'
+                    placeholder='darthvader@empire.com'
+                    size='md'
+                  />
+                </Box>
+                <Box>
+                  <FormLabel>Message</FormLabel>
+                  <Textarea
+                    type='text'
+                    name='message'
+                    placeholder='Join the dark side.'
+                    size='md'
+                  />
+                </Box>
+                {status === 'SUCCESS' ? (
+                  <FormHelperText>Thanks!</FormHelperText>
+                ) : (
+                  <Input
+                    type='submit'
+                    h='48px'
+                    w={['50%', '40%', '30%', '25%']}
+                    borderRadius='8px'
+                    borderColor='none'
+                    backgroundColor='brand.900'
+                    color='white'
+                    mt='3'
+                    _hover={{ backgroundColor: 'red.700' }}
+                  />
+                )}
+                {status === 'ERROR' && (
+                  <FormErrorMessage>
+                    Ooops! There was an error.
+                  </FormErrorMessage>
+                )}
+              </Stack>
+            </FormControl>
+          </form>
+        </Box>
       </Flex>
     </div>
   );
