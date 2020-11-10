7ce90a658284f7676578dcec3c481a4d7c0ab186
maximdegeyter <maximdegeyter@outlook.com>
2020-11-11 00:19:34 +0100

    edit projects to use contentful

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
-import { Flex, Heading, Text, Box } from '@chakra-ui/core';
+import { Flex, Heading, Box } from '@chakra-ui/core';
+import Project from '../components/project';
+
+const client = require('contentful').createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+});
 
 export default function Projects() {
+  async function fetchEntries() {
+    const entries = await client.getEntries();
+    if (entries.items) return entries.items;
+    console.log(`Error getting Entries for ${contentType.name}.`);
+  }
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function getPosts() {
+      const allPosts = await fetchEntries();
+      setPosts([...allPosts]);
+    }
+    getPosts();
+  }, []);
+
   return (
     <div>
       <Head>
@@ -17,11 +40,21 @@ export default function Projects() {
         mx='auto'
         my='16'
       >
-        <Heading as='h1' size='xl' mb='8' color='text.900'>
+        <Heading as='h2' size='xl' mb='8' color='text.900'>
           Projects.
         </Heading>
         <Box w={['100%', '100%', '100%', '720px']} mx='auto'>
-          <Text>This is still being developed with Contentful CMS.</Text>
+          {posts.length > 0
+            ? posts.map((p) => (
+                <Project
+                  title={p.fields.title}
+                  desc={p.fields.description}
+                  tags={p.fields.tags}
+                  img={p.fields.image}
+                  url={p.fields.url}
+                />
+              ))
+            : null}
         </Box>
       </Flex>
     </div>
